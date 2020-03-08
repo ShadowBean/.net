5d851b304de5f43e514cca3576c265dfc0e6b3ed
ShadowBean <61884525+ShadowBean@users.noreply.github.com>
2020-03-08 20:08:05 +0100

    3.6

---
+++ MailSender/MailSender/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MailSender
+{
+    partial class MailSenderForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MailSenderForm));
+            this.From = new System.Windows.Forms.TextBox();
+            this.To = new System.Windows.Forms.TextBox();
+            this.STMPServer = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Subject = new System.Windows.Forms.TextBox();
+            this.Mail = new System.Windows.Forms.TextBox();
+            this.AttachmentButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.openFileDialogAttachment = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // From
+            // 
+            this.From.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.From.Location = new System.Drawing.Point(12, 12);
+            this.From.Name = "From";
+            this.From.Size = new System.Drawing.Size(228, 20);
+            this.From.TabIndex = 0;
+            this.From.Text = "From";
+            this.From.Enter += new System.EventHandler(this.From_Enter);
+            this.From.Leave += new System.EventHandler(this.From_Leave);
+            // 
+            // To
+            // 
+            this.To.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.To.Location = new System.Drawing.Point(12, 38);
+            this.To.Name = "To";
+            this.To.Size = new System.Drawing.Size(228, 20);
+            this.To.TabIndex = 1;
+            this.To.Text = "To";
+            this.To.Enter += new System.EventHandler(this.To_Enter);
+            this.To.Leave += new System.EventHandler(this.To_Leave);
+            // 
+            // STMPServer
+            // 
+            this.STMPServer.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.STMPServer.Location = new System.Drawing.Point(12, 64);
+            this.STMPServer.Name = "STMPServer";
+            this.STMPServer.Size = new System.Drawing.Size(228, 20);
+            this.STMPServer.TabIndex = 2;
+            this.STMPServer.Text = "smtp.wp.pl";
+            this.STMPServer.Enter += new System.EventHandler(this.STMPServer_Enter);
+            this.STMPServer.Leave += new System.EventHandler(this.STMPServer_Leave);
+            // 
+            // Username
+            // 
+            this.Username.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Username.Location = new System.Drawing.Point(12, 90);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(228, 20);
+            this.Username.TabIndex = 3;
+            this.Username.Text = "Username";
+            this.Username.Enter += new System.EventHandler(this.Username_Enter);
+            this.Username.Leave += new System.EventHandler(this.Username_Leave);
+            // 
+            // Password
+            // 
+            this.Password.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Password.Location = new System.Drawing.Point(12, 116);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(228, 20);
+            this.Password.TabIndex = 4;
+            this.Password.Text = "Password";
+            this.Password.Enter += new System.EventHandler(this.Password_Enter);
+            this.Password.Leave += new System.EventHandler(this.Password_Leave);
+            // 
+            // Subject
+            // 
+            this.Subject.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Subject.Location = new System.Drawing.Point(12, 142);
+            this.Subject.Name = "Subject";
+            this.Subject.Size = new System.Drawing.Size(228, 20);
+            this.Subject.TabIndex = 5;
+            this.Subject.Text = "Subject";
+            this.Subject.Enter += new System.EventHandler(this.Subjent_Enter);
+            this.Subject.Leave += new System.EventHandler(this.Subject_Leave);
+            // 
+            // Mail
+            // 
+            this.Mail.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Mail.Location = new System.Drawing.Point(12, 168);
+            this.Mail.Multiline = true;
+            this.Mail.Name = "Mail";
+            this.Mail.Size = new System.Drawing.Size(228, 176);
+            this.Mail.TabIndex = 6;
+            this.Mail.Text = "Message";
+            this.Mail.Enter += new System.EventHandler(this.Message_Enter);
+            this.Mail.Leave += new System.EventHandler(this.Message_Leave);
+            // 
+            // AttachmentButton
+            // 
+            this.AttachmentButton.Location = new System.Drawing.Point(12, 350);
+            this.AttachmentButton.Name = "AttachmentButton";
+            this.AttachmentButton.Size = new System.Drawing.Size(75, 23);
+            this.AttachmentButton.TabIndex = 7;
+            this.AttachmentButton.Text = "Attachment";
+            this.AttachmentButton.UseVisualStyleBackColor = true;
+            this.AttachmentButton.Click += new System.EventHandler(this.AttachmentButton_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(165, 350);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 8;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // openFileDialogAttachment
+            // 
+            this.openFileDialogAttachment.FileName = "openFileDialogAttachment";
+            // 
+            // MailSenderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 388);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.AttachmentButton);
+            this.Controls.Add(this.Mail);
+            this.Controls.Add(this.Subject);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.STMPServer);
+            this.Controls.Add(this.To);
+            this.Controls.Add(this.From);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MailSenderForm";
+            this.Text = "Mail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox From;
+        private System.Windows.Forms.TextBox To;
+        private System.Windows.Forms.TextBox STMPServer;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.TextBox Subject;
+        private System.Windows.Forms.TextBox Mail;
+        private System.Windows.Forms.Button AttachmentButton;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialogAttachment;
+    }
+}
+
---
+++ MailSender/MailSender/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.IO;
+
+namespace MailSender
+{
+    public partial class MailSenderForm : Form
+    {
+        string file = null;
+
+        public MailSenderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void From_Enter(object sender, EventArgs e)
+        {
+            if (From.Text == "From")
+            {
+                From.Text = "";
+                From.ForeColor = Color.Black;
+            }
+        }
+
+        private void To_Enter(object sender, EventArgs e)
+        {
+            if (To.Text == "To")
+            { 
+                To.Text = "";
+                To.ForeColor = Color.Black;
+            }
+        }
+
+        private void STMPServer_Enter(object sender, EventArgs e)
+        {
+            if (STMPServer.Text == "STMPServer")
+            {
+                STMPServer.Text = "";
+                STMPServer.ForeColor = Color.Black;
+            }
+        }
+
+        private void Username_Enter(object sender, EventArgs e)
+        {
+            if (Username.Text == "Username")
+            {
+                Username.Text = "";
+                Username.ForeColor = Color.Black;
+            }
+        }
+
+        private void Subjent_Enter(object sender, EventArgs e)
+        {
+            if (Subject.Text == "Subject")
+            {
+                Subject.Text = "";
+                Subject.ForeColor = Color.Black;
+            }
+        }
+
+        private void Message_Enter(object sender, EventArgs e)
+        {
+            if (Mail.Text == "Message")
+            {
+                Mail.Text = "";
+                Mail.ForeColor = Color.Black;
+            }
+        }
+
+        private void Password_Enter(object sender, EventArgs e)
+        {
+            if (Password.Text == "Password")
+            {
+                Password.Text = "";
+                Password.ForeColor = Color.Black;
+                Password.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void From_Leave(object sender, EventArgs e)
+        {
+            if (From.Text == "")
+            {
+                From.Text = "From";
+                From.ForeColor = Color.Silver;
+            }
+        }
+
+        private void To_Leave(object sender, EventArgs e)
+        {
+            if (To.Text == "")
+            {
+                To.Text = "To";
+                To.ForeColor = Color.Silver;
+            }
+        }
+
+        private void STMPServer_Leave(object sender, EventArgs e)
+        {
+            if (STMPServer.Text == "")
+            {
+                STMPServer.Text = "STMPServer";
+                STMPServer.ForeColor = Color.Silver;
+            }
+        }
+
+        private void Username_Leave(object sender, EventArgs e)
+        {
+            if (Username.Text == "")
+            {
+                Username.Text = "Username";
+                Username.ForeColor = Color.Silver;
+            }
+            
+        }
+
+        private void Password_Leave(object sender, EventArgs e)
+        {
+            if (Password.Text == "")
+            {
+
+                Password.Text = "Password";
+                Password.ForeColor = Color.Silver;
+                Password.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void Subject_Leave(object sender, EventArgs e)
+        {
+            if (Subject.Text == "")
+            {
+                Subject.Text = "Subject";
+                Subject.ForeColor = Color.Silver;
+            }
+        }
+
+        private void Message_Leave(object sender, EventArgs e)
+        {
+            if (Mail.Text == "")
+            {
+                Mail.Text = "Message";
+                Mail.ForeColor = Color.Silver;
+            }
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage(From.Text, To.Text, Subject.Text, Mail.Text);
+            SmtpClient client = new SmtpClient(STMPServer.Text)
+            {
+                Port = 587,
+                Credentials = new System.Net.NetworkCredential(Username.Text, Password.Text),
+                EnableSsl = true
+            };
+            if(!string.IsNullOrEmpty(file))
+            {
+                System.Net.Mail.Attachment attachment;
+                attachment = new System.Net.Mail.Attachment(file);
+                mail.Attachments.Add(attachment);
+            }
+           
+
+
+            client.Send(mail);
+            MessageBox.Show("Mail sent", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void AttachmentButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialogAttachment.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                file = openFileDialogAttachment.FileName;
+            }
+            
+        }
+    }
+}
