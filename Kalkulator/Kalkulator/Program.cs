3c080b390f859227513b678a78a7da794da4d68b
ShadowBean <61884525+ShadowBean@users.noreply.github.com>
2020-03-17 00:59:52 +0100

    1.5

---
+++ Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using KalkulatorLib;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("1+2=" + Calculate.Add(1, 2));
+            Console.WriteLine("1-2=" + Calculate.Sub(1, 2));
+            Console.WriteLine("1*2=" + Calculate.Mul(1, 2));
+            Console.WriteLine("1/2=" + Calculate.Div(1, 2));
+        }
+    }
+}
