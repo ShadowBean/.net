0af121a7eac3b5941891aa8e9c876995774b492e
ShadowBean <61884525+ShadowBean@users.noreply.github.com>
2020-03-19 11:33:07 +0100

    4.2

---
+++ Zegary/ClockControl/AnalogDigitClock.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ClockControl
+{
+    partial class AnalogDigitClock
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Digit = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.backBox = new System.Windows.Forms.PictureBox();
+            this.dotBox = new System.Windows.Forms.PictureBox();
+            this.hourBox = new System.Windows.Forms.PictureBox();
+            this.minuteBox = new System.Windows.Forms.PictureBox();
+            this.secondBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.backBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dotBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hourBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Digit
+            // 
+            this.Digit.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Digit.Location = new System.Drawing.Point(3, 0);
+            this.Digit.Name = "Digit";
+            this.Digit.Size = new System.Drawing.Size(282, 71);
+            this.Digit.TabIndex = 0;
+            this.Digit.Text = "--:--   --";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // backBox
+            // 
+            this.backBox.BackColor = System.Drawing.Color.Transparent;
+            this.backBox.Location = new System.Drawing.Point(14, 74);
+            this.backBox.Name = "backBox";
+            this.backBox.Size = new System.Drawing.Size(312, 309);
+            this.backBox.TabIndex = 1;
+            this.backBox.TabStop = false;
+            // 
+            // dotBox
+            // 
+            this.dotBox.BackColor = System.Drawing.Color.Transparent;
+            this.dotBox.Location = new System.Drawing.Point(14, 74);
+            this.dotBox.Name = "dotBox";
+            this.dotBox.Size = new System.Drawing.Size(312, 309);
+            this.dotBox.TabIndex = 2;
+            this.dotBox.TabStop = false;
+            // 
+            // hourBox
+            // 
+            this.hourBox.BackColor = System.Drawing.Color.Transparent;
+            this.hourBox.Location = new System.Drawing.Point(14, 74);
+            this.hourBox.Name = "hourBox";
+            this.hourBox.Size = new System.Drawing.Size(312, 309);
+            this.hourBox.TabIndex = 3;
+            this.hourBox.TabStop = false;
+            // 
+            // minuteBox
+            // 
+            this.minuteBox.BackColor = System.Drawing.Color.Transparent;
+            this.minuteBox.Location = new System.Drawing.Point(14, 74);
+            this.minuteBox.Name = "minuteBox";
+            this.minuteBox.Size = new System.Drawing.Size(312, 309);
+            this.minuteBox.TabIndex = 4;
+            this.minuteBox.TabStop = false;
+            // 
+            // secondBox
+            // 
+            this.secondBox.BackColor = System.Drawing.Color.Transparent;
+            this.secondBox.Location = new System.Drawing.Point(14, 74);
+            this.secondBox.Name = "secondBox";
+            this.secondBox.Size = new System.Drawing.Size(312, 309);
+            this.secondBox.TabIndex = 5;
+            this.secondBox.TabStop = false;
+            // 
+            // AnalogDigitClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.secondBox);
+            this.Controls.Add(this.minuteBox);
+            this.Controls.Add(this.hourBox);
+            this.Controls.Add(this.dotBox);
+            this.Controls.Add(this.backBox);
+            this.Controls.Add(this.Digit);
+            this.Name = "AnalogDigitClock";
+            this.Size = new System.Drawing.Size(354, 401);
+            ((System.ComponentModel.ISupportInitialize)(this.backBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dotBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hourBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Digit;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.PictureBox backBox;
+        private System.Windows.Forms.PictureBox dotBox;
+        private System.Windows.Forms.PictureBox hourBox;
+        private System.Windows.Forms.PictureBox minuteBox;
+        private System.Windows.Forms.PictureBox secondBox;
+    }
+}
---
+++ Zegary/ClockControl/AnalogDigitClock.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClockControl.Properties;
+
+namespace ClockControl
+{
+    public partial class AnalogDigitClock: UserControl
+    {
+
+        readonly List<Bitmap> Images = new List<Bitmap>()
+        {
+             Resources.back,
+             Resources.dot,
+             Resources.hour,
+             Resources.minute,
+             Resources.second
+        };
+
+        public AnalogDigitClock()
+        {
+            InitializeComponent();
+            Digit.Text = DateTime.Now.ToString("HH:mm   ss");
+
+            Pictures();
+
+            Timer.Start();
+        }
+        void Pictures()
+        {
+            backBox.Image = Images[0];
+            backBox.Controls.Add(hourBox);
+          
+
+            hourBox.Controls.Add(minuteBox);
+            hourBox.Location = new Point(0, 0);
+
+            minuteBox.Controls.Add(dotBox);
+            minuteBox.Location = new Point(0, 0);
+
+            dotBox.Image = Images[1];
+            dotBox.Location = new Point(0, 0);
+
+            dotBox.Controls.Add(secondBox);
+            secondBox.Location = new Point(0, 0);
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Digit.Text = DateTime.Now.ToString("HH:mm   ss");
+
+            DateTime Now = DateTime.Now;
+            int Hour = Now.Hour;
+            int Minute = Now.Minute;
+            int Second = Now.Second;
+
+            Single AngleS = Second * 6;
+            Single AngleM = Minute * 6 + AngleS / 60;
+            Single AngleH = Hour * 30 + AngleM / 12;
+
+            hourBox.Image = RotateImage(Images[2], AngleH);
+            minuteBox.Image = RotateImage(Images[3], AngleM);
+            secondBox.Image = RotateImage(Images[4], AngleS);
+
+        }
+
+        private Bitmap RotateImage(Bitmap rotateMe, float angle)
+        {
+            Bitmap rotatedImage = new Bitmap(rotateMe.Width, rotateMe.Height);
+
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                g.TranslateTransform(rotateMe.Width / 2, rotateMe.Height / 2);
+                g.RotateTransform(angle);
+                g.TranslateTransform(-rotateMe.Width / 2, -rotateMe.Height / 2);
+                g.DrawImage(rotateMe, new Point(0, 0));
+            }
+
+            return rotatedImage;
+        }
+
+        
+    }
+}
